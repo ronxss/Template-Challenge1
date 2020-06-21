cfeb3e971f625700f27ce31b3deb9b7d2103a510
= <=>
2020-06-21 01:41:16 -0300

    Desafio 1 NodeJS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+ const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,81 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+      return response.status(400).json({error: 'Invalid project ID.'});
+  }
+
+  return next();
+}
+
+app.get('/repositories', (request, response) => {
+  console.log('Able to list the repositories');
+  return response.json(repositories);
+
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  console.log('Able to create a new repository');
+  return response.json(repository);
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+const repositoryIndex = repositories.findIndex(repository => repository.id == id)
+
+if (repositoryIndex < 0) {
+  console.log('Not able to update a repository that does not exists');
+  return response.status(400).json({ error: 'Repository not found.'});
+}
+
+const repository = {
+  id,
+  title,
+  url,
+  techs,
+  likes: repositories.find(repository => repository.likes)
+}
+
+repositories[repositoryIndex]  = repository;
+  console.log('Able to update a repository');
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id == id)
+
+if (repositoryIndex < 0) {
+  console.log('Not able to delete a repository that does not exists')
+  return response.status(400).json({ error: 'Repository not found.'})
+}
+
+repositories.splice(repositoryIndex, 1);
+
+return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = repositories.find(repository => repository.id == id);
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
